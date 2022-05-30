47bae693f2dc7ee2cff6de480acf24b05d792d87
geraldik <geraldik1986@gmail.com>
2022-05-30 23:04:13 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(9000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    System.out.print("\r Loading ...\\");
+                    Thread.sleep(100);
+                    System.out.print("\r Loading ...|");
+                    Thread.sleep(100);
+                    System.out.print("\r Loading .../");
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
